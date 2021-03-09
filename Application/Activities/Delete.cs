9ee35fc0e0706f9862bceec4f24740f413d8f1d3
datngo <ngohavandat93ndc@gmail.com>
2021-03-09 21:57:17 +0700

    Update Edit, Delete and Mapper

--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -1,35 +1,33 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
-using AutoMapper;
-using Domain;
 using MediatR;
 using Persistence;
 
 namespace Application.Activities
 {
-    public class Edit
+    public class Delete
     {
         public class Command : IRequest
         {
-            public Activity Activity { get; set; }
-
+            public Guid Id { get; set; }
         }
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            private readonly IMapper _mapper;
-            public Handler(DataContext context, IMapper mapper)
+            public Handler(DataContext context)
             {
-                _mapper = mapper;
                 _context = context;
-
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
-                var activity = await _context.Activities.FindAsync(request.Activity.Id);
-                _mapper.Map(request.Activity, activity);
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                _context.Remove(activity);
+
                 await _context.SaveChangesAsync();
+
                 return Unit.Value;
             }
         }
