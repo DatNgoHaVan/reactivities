0df6827e7d5a14466ee79cd4ef7021c938cc1bdd
datngo <ngohavandat93ndc@gmail.com>
2021-03-20 16:40:12 +0700

    Update identity and authen

--- API/Program.cs
+++ API/Program.cs
@@ -25,8 +25,9 @@ namespace API
             try
             {
                 var context = services.GetRequiredService<DataContext>();
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
                 await context.Database.MigrateAsync();
-                await Seed.SeedData(context);
+                await Seed.SeedData(context, userManager);
             }
             catch (Exception ex)
             {
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,13 +1,13 @@
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
-
         }
 
         public DbSet<Activity> Activities { get; set; }
